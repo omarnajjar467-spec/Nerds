f2a150f0ca4a84c54df0c733a30e235425130865
ooooomar <omarnajjar467@gmail.com>
2025-11-13 10:05:45 +0100

    Add navigation bar to nav.php

---
+++ nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stijl.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Navigation -->
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="activiteit.php">Activiteiten</a></li>
+            <li><a href="student.php">Studenten</a></li>
+            <li><a href="foto.php">Foto's</a></li>
+            <li><a href="score.php">Score</a></li>
+            <li><a href="reactie.php">Reactie</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+
+
+    
+</body>
+</html>
