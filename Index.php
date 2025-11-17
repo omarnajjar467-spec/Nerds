1b5c0a78e1dd607b5bc7856cd6c3da6a3a601e80
pointedinsanity <mylanroelandts@gmail.com>
2025-11-17 11:14:26 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -29,21 +29,7 @@ else {
     
     
 <?php 
-switch (expression) {
-  case label1:
-    //code block
-    break;
-  case label2:
-    //code block;
-    break;
-  case label3:
-    //code block
-    break;
-  default:
-    //code block
-}
 
-?>
 
     
 
